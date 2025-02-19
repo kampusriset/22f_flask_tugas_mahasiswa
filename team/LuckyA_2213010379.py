0f6e00422312552654fd8ab7d4fa56b67c603ade
1uckyamd <169018048+1uckyamd@users.noreply.github.com>
2025-02-19 21:08:44 +0700

    Create LuckyA_2213010379.py

---
+++ team/LuckyA_2213010379.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo Perkenalkan, nama saya ", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Lucky Amadya Falsabila"
+    nim = "2213010379"
+    peran = "Mengerjakan flowchart, edit video dan upload youtube"
+    perkenalan(nama, nim, peran)
