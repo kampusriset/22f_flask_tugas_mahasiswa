d6138f6f4dbc39d47088a06847a23205a4e4ecda
RyandraSN <161477222+RyandraSN@users.noreply.github.com>
2025-02-19 21:06:02 +0700

    Create RyandraSN_2213010344.py

---
+++ team/RyandraSN_2213010344.py
@@ -0,0 +1,10 @@
+def perkenalan (nama, nim, peran):
+    print ("Perkenalkan, Saya ", nama)
+    print ("NIM : ", nim)
+    print ("Peran saya yaitu : ", peran)
+    
+if __name__ == "__main__":
+    nama = "Ryandra Surya Nandika"
+    nim = "2213010344"
+    peran = "Ketua"
+    perkenalan(nama, nim, peran)
